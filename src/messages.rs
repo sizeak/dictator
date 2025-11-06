59b8bf192023ac0f4224994050e7de42eeb2c535
Simon Jackson <simon.jackson@sonocent.com>
2025-11-06 14:14:01 +0000

    Remove unnecessary comments from RecorderCommand and AppState enums

--- src/messages.rs
+++ src/messages.rs
@@ -2,13 +2,11 @@ use anyhow::Result;
 use tempfile::NamedTempFile;
 use tokio::sync::oneshot;
 
-/// Commands for the Recorder service
 pub enum RecorderCommand {
     Start,
     Stop(oneshot::Sender<Result<NamedTempFile>>),
 }
 
-/// Application state (observable via watch channel)
 #[derive(Clone, Debug, PartialEq)]
 pub enum AppState {
     Idle,
